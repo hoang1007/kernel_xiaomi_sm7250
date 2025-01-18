2900f7f386c280aef1e7806542b9795785a02dc2
Hoang <hoangvuhuy1007@gmail.com>
2025-01-18 18:05:37 +0000

    fs: erofs: Use external sched_set_fifo()
    Signed-off-by: Hoang <hoangvuhuy1007@gmail.com>

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -7,6 +7,7 @@
 #include "compress.h"
 #include <linux/cpuhotplug.h>
 #include <linux/kthread.h>
+#include <linux/sched.h>
 #include <uapi/linux/sched/types.h>
 #include <trace/events/erofs.h>
 
@@ -388,21 +389,6 @@ static void erofs_destroy_percpu_workers(void)
 			kthread_destroy_worker(worker);
 	}
 	kfree(z_erofs_pcpu_workers);
-}
-
-void sched_set_normal(struct task_struct *p, int nice)
-{
-	struct sched_attr attr = {
-		.sched_policy = SCHED_NORMAL,
-		.sched_nice = nice,
-	};
-	WARN_ON_ONCE(sched_setattr_nocheck(p, &attr) != 0);
-}
-
-void sched_set_fifo_low(struct task_struct *p)
-{
-	struct sched_param sp = { .sched_priority = 1 };
-	WARN_ON_ONCE(sched_setscheduler_nocheck(p, SCHED_FIFO, &sp) != 0);
 }
 
 static struct kthread_worker *erofs_init_percpu_worker(int cpu)
